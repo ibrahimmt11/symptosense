252aa052d9d8b1f41832461ef11b903ade166271
ibrahimmt11 <abitmulyo@gmail.com>
2024-03-28 19:42:43 +0700

    landing *tinggal rapihin

--- symptosense/resources/views/Landing/landing.blade.php
+++ symptosense/resources/views/Landing/landing.blade.php
@@ -86,16 +86,16 @@
         </div>
         <div class="col-md-3">
           <div class="card">
-            <img src="https://via.placeholder.com/300" class="card-img-top" alt="Placeholder Image">
+            <img src="https://images.unsplash.com/photo-1529386317747-0a2a51add902?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="Placeholder Image">
             <div class="card-body">
               <h5 class="card-title">Card 2</h5>
-              <p class="card-text">Ini adalah deskripsi dari card 2.</p>
+              <p class="card-text">Ini adalah deskripsi dari card 2. </p>
             </div>
           </div>
         </div>
         <div class="col-md-3">
           <div class="card">
-            <img src="https://via.placeholder.com/300" class="card-img-top" alt="Placeholder Image">
+            <img src="https://images.unsplash.com/photo-1624031000106-79254a8faa19?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="Placeholder Image">
             <div class="card-body">
               <h5 class="card-title">Card 3</h5>
               <p class="card-text">Ini adalah deskripsi dari card 3.</p>
@@ -104,7 +104,7 @@
         </div>
         <div class="col-md-3">
           <div class="card">
-            <img src="https://via.placeholder.com/300" class="card-img-top" alt="Placeholder Image">
+            <img src="https://images.unsplash.com/photo-1518964392857-88332d1cf619?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="Placeholder Image">
             <div class="card-body">
               <h5 class="card-title">Card 4</h5>
               <p class="card-text">Ini adalah deskripsi dari card 4.</p>
@@ -113,9 +113,75 @@
         </div>
       </div>
     </div>
-
   </div>
 
+  <div class="row pt-5 mx-5">
+    <div class="col-md-12 text-center">
+      <p class="artikel pt-4">Obat & Vitamin</p>
+    </div>
+    <div class="container pt-3 ms-md-auto text-center obat">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body d-flex flex-column align-items-center">
+              <img src="assets/images/gatal2.png" class="card-img-top" alt="Gambar Gatal">
+              <p class="card-text pt-3">Gatal</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body d-flex flex-column align-items-center">
+              <img src="assets/images/batuk.png" class="card-img-top" alt="Gambar Batuk">
+              <p class="card-text pt-3">Batuk</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body d-flex flex-column align-items-center">
+              <img src="assets/images/pencernaan.png" class="card-img-top" alt="Gambar Pencernaan">
+              <p class="card-text pt-3">Pencernaan</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body d-flex flex-column align-items-center">
+              <img src="assets/images/pusing.png" class="card-img-top" alt="Gambar Pusing">
+              <p class="card-text pt-3">Pusing</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer">
+    <footer class="text-white pt-5 pb-4">
+      <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left">
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weigth-bold">Symptosense</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weigth-bold">Product</h5>
+          </div>
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weigth-bold">Usefull link</h5>
+          </div>
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 class="text-uppercase mb-4 font-weigth-bold">Contact</h5>
+          </div>
+
+        </div>
+      </div>
+
+    </footer>
+  </div>
 
 </body>
 
