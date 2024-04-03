d1d0884dd3c8cd619f39d8b12ea81fe9c867b0b7
ibrahimmt11 <abitmulyo@gmail.com>
2024-04-03 23:21:50 +0700

    perbaikan diagnosis

--- symptosense/resources/views/Diagnosis/diagnosis.blade.php
+++ symptosense/resources/views/Diagnosis/diagnosis.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="assets/css/diagnosis.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     <title>Diagnosis</title>
 </head>
 
@@ -60,61 +60,52 @@
                 <p class="info-text">Isi gejala yang anda alami!!</p>
             </div>
             <div class="container text-center">
-                <div class="row g-2">
-                    <div class="col-6">
-                        <div class="btn-group">
-                            <button class="btn dropdown-toggle" type="button">
-                                Pilih gejala anda
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Gejala 1</a></li>
-                                <li><a class="dropdown-item" href="#">Gejala 2</a></li>
-                                <li><a class="dropdown-item" href="#">Gejala 3</a></li>
-                            </ul>
+                <form action="">
+                    <div class="row g-2">
+                        <div class="col-6">
+                            <select name="" id="" class="form-select">
+                                <option selected disabled value="">Pilih gejala anda</option>
+                                <option value="b">gejala 1</option>
+                                <option value="c">gejala 2</option>
+                                <option value="d">gejala 3</option>
+                            </select>
                         </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="btn-group">
-                            <button class="btn dropdown-toggle" type="button">
-                                Pilih gejala anda
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Gejala 1</a></li>
-                                <li><a class="dropdown-item" href="#">Gejala 2</a></li>
-                                <li><a class="dropdown-item" href="#">Gejala 3</a></li>
-                            </ul>
+                        <div class="col-6">
+                            <select name="" id="" class="form-select">
+                                <option selected disabled value="">Pilih gejala anda</option>
+                                <option value="b">gejala 1</option>
+                                <option value="c">gejala 2</option>
+                                <option value="d">gejala 3</option>
+                            </select>
                         </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="btn-group">
-                            <button class="btn dropdown-toggle" type="button">
-                                Pilih gejala anda
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Gejala 1</a></li>
-                                <li><a class="dropdown-item" href="#">Gejala 2</a></li>
-                                <li><a class="dropdown-item" href="#">Gejala 3</a></li>
-                            </ul>
+                        <div class="col-6">
+                            <select name="" id="" class="form-select">
+                                <option selected disabled value="">Pilih gejala anda</option>
+                                <option value="b">gejala 1</option>
+                                <option value="c">gejala 2</option>
+                                <option value="d">gejala 3</option>
+                            </select>
                         </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="row">
-                            <div class="col-6">
-                                <button type="button" class="btn btn-tambah">
-                                    <i class="fas fa-plus"></i> Tambah Gejala
-                                </button>
-                            </div>
-                            <div class="col-6">
-                                <button type="button" class="btn btn-hasil">
-                                    <i class="fas fa-eye"></i> Lihat Hasil Gejala
-                                </button>
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-6">
+                                    <button type="button" class="btn btn-tambah">
+                                        <i class="fas fa-plus"></i> Tambah Gejala
+                                    </button>
+                                </div>
+                                <div class="col-6">
+                                    <button type="button" class="btn btn-hasil">
+                                        <i class="fas fa-eye"></i> Lihat Hasil Gejala
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
