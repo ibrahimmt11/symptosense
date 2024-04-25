cd0f7c5a9b528c1bea3074ea463513f75827000a
Arzaq Ajradika <arzaqajradika1@gmail.com>
2024-04-26 01:29:49 +0700

    migration update

--- symptosense/database/migrations/2024_03_28_131553_create_pasien_table.php
+++ symptosense/database/migrations/2024_03_28_131553_create_pasien_table.php
@@ -15,17 +15,17 @@ return new class extends Migration
             $table->id('id_pasien');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->string('nama_lengkap')->nullable();
+            $table->string('user_nama')->nullable();
+            $table->foreign('user_nama')->references('name')->on('users'); // Assuming 'name' is now unique
             $table->char('jenis_kelamin', 1)->nullable();
             $table->date('tgl_lahir')->nullable();
             $table->string('no_telp')->nullable();
             $table->string('email')->unique();
-            $table->text('alamat')->nullable();;
-            $table->decimal('tinggi_badan', 5, 2)->nullable();; 
-            $table->decimal('berat_badan', 5, 2)->nullable();; 
-            $table->string('NIK')->unique()->nullable();;
-            $table->timestamps(); 
-            
+            $table->text('alamat')->nullable();
+            $table->decimal('tinggi_badan', 5, 2)->nullable();
+            $table->decimal('berat_badan', 5, 2)->nullable();
+            $table->string('NIK')->unique()->nullable();
+            $table->timestamps();
         });
     }
 
@@ -36,4 +36,5 @@ return new class extends Migration
     {
         Schema::dropIfExists('pasien');
     }
+    
 };
